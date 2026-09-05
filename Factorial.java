28b7bc226a8a99c6bc95e17d9153d759599572b2
rohinibiradar7482-maker <rohinibiradar7482@gmail.com>
2026-09-05 10:07:30 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,17 @@
 public class Factorial {
-    public static void main(String[] args) {
-        int n = 5;
-        int factorial = 1;
+    static int factorial(int n) {
+        int fact = 1;
 
         for (int i = 1; i <= n; i++) {
-            factorial = factorial * i;
+            fact = fact * i;
         }
 
-        System.out.println("Factorial = " + factorial);
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Factorial = " + factorial(n));
     }
 }
